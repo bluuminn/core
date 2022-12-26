168c96c2a1b624f86b4d1b2768a7aad7e89438af
River Kim <bluuminn@gmail.com>
2022-12-26 23:18:43 +0900

    회원 도메인 개발

---
+++ src/main/java/com/bluuminn/core/member/MemberServiceImpl.java
@@ -0,0 +1,16 @@
+package com.bluuminn.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository repository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        repository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return repository.findById(memberId);
+    }
+}
